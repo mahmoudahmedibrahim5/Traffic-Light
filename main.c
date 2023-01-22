d81af3aebc76513de85a0451330ebad95d2722b7
Mahmoud Ahmed <82950581+mahmoudahmedibrahim5@users.noreply.github.com>
2023-01-23 00:15:41 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * Traffic Light.c
+ *
+ * Created: 09-Dec-22 7:23:58 PM
+ * Author : Mahmoud Ahmed
+ */ 
+
+#include "Application/APP.h"
+
+int main(void)
+{
+	initialize();
+    while (1) 
+    {
+		NormalMode();		
+    }
+}
+
+
